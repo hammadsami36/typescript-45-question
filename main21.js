e818681cb02c067d556dc503f582a9746309b68e
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:40:30 +0500

    Add files via upload

---
+++ main21.js
@@ -0,0 +1,11 @@
+// two object
+var vegetables = {
+    name: "onion",
+    price: 100
+};
+var fruit = {
+    name: "Apple",
+    price: 200
+};
+console.log("vegetables name: ".concat(vegetables.name, ", vegetables price: ").concat(vegetables.price));
+console.log("fruit name: ".concat(fruit.name, ", fruit price: ").concat(fruit.price));
---
+++ main21.ts
@@ -0,0 +1,16 @@
+interface item {
+    name : string
+    price : number
+}
+// two object
+const vegetables: item = {
+name: `onion`,
+price: 100
+}
+
+const fruit: item = {
+    name: `Apple`,
+    price: 200
+    }
+    console.log(`vegetables name: ${vegetables.name}, vegetables price: ${vegetables.price}`)
+    console.log(`fruit name: ${fruit.name}, fruit price: ${fruit.price}`)
