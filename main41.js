98e5a3c45eceb0ed32c5424617a085bf58944fa2
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:54:58 +0500

    Add files via upload

---
+++ main41.js
@@ -0,0 +1,7 @@
+var Magician_list = ["dynamo", "roy horn", "david copper field", "david blaine"];
+for (var i = 0; i < Magician_list.length; i++) {
+    function call_magician(list) {
+        console.log(list);
+    }
+    console.log(Magician_list);
+}
---
+++ main41.ts
@@ -0,0 +1,7 @@
+let Magician_list : string[] = ["dynamo","roy horn","david copper field","david blaine"]
+for (let i=0; i<Magician_list.length; i++){
+    function call_magician(list:string,){
+        console.log(list)
+    }
+    console.log(Magician_list)
+}
