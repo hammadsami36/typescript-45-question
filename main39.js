aa249e2badcd707eeb23bc0619f37f2aee2713f1
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:53:54 +0500

    Add files via upload

---
+++ main39.js
@@ -0,0 +1,9 @@
+var lahore = "lahore ,Pakistan";
+var Istambul = "Istambul , turkey";
+var Dehli = "Dehli , india";
+function city_name(city) {
+    console.log(city);
+}
+console.log(lahore);
+console.log(Istambul);
+console.log(Dehli);
---
+++ main39.ts
@@ -0,0 +1,10 @@
+let lahore = "lahore ,Pakistan"
+let Istambul = "Istambul , turkey"
+let Dehli = "Dehli , india"
+
+function city_name(city:string,){
+    console.log(city)
+}
+console.log(lahore)
+console.log(Istambul)
+console.log(Dehli)
