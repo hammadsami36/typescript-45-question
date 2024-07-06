a173e53d37ac4e6cc38a6234f734e869174b4b85
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:38:05 +0500

    Add files via upload

---
+++ main18.ts
@@ -0,0 +1,18 @@
+let places: string[] = [ `Islamabad` ,`beljing` ,`sydney`, `new york` ,`paris` ];
+
+// Print your array in its original order.
+console.log(`orginal`  + places);
+// Print your array in alphabetical order without modifying the actual list.
+console.log(`copy`  +[...places].sort());
+// Show that your array is still in its original order by printing it.
+console.log(`orginal`  + places);
+// Print your array in reverse alphabetical order without changing the order of the original list.
+console.log(`copy`  +[...places].sort().reverse());
+// Show that your array is still in its original order by printing it again.
+console.log(`copy`  +[...places].sort().reverse());
+// Reverse the order of your list. Print the array to show that its order has changed.
+console.log(`copy`  +[...places].sort());
+// Reverse the order of your list again. Print the list to show it’s back to its original order.
+console.log(`copy` +[...places].sort().reverse());
+// Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log(`copy` +[...places].sort());
