913f9f522372ffefa7c554ceb7aaa7e85489dd26
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:39:04 +0500

    Add files via upload

---
+++ main19.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var module_1 = require("../14 .Guest_list/main");
+console.log("number of guests");
+console.log("we have succsefully imported ".concat(module_1.Guest_list, "from question 14 . "));
---
+++ main19.ts
@@ -0,0 +1,3 @@
+import{Guest_list} from `../14 .Guest_list/main`
+console.log(`number of guests`);
+console.log(`we have succsefully imported ${Guest_list}from question 14 . `);
