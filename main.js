e703436248d283767c1fb4fb3990f864e7df2d46
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:35:06 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ exports.guest_list = guest_list;
 for (var i = 0; i < guest_list.length; i++) {
     console.log("Dear " + guest_list[i] + "\n\nYou are invited for a dinner tommorrow. \n\nThank you. \n\n");
 }
+// let not_coming: string = `maryam`;
+// let new_guest: string = `ayesha`;
+// guest_list[0] = new_guest;
+// for(let i=0; i< guest_list.length; i++){
+//     console.log(`Dear ` + guest_list[i] +`\n\nYou are invited for a dinner tommorrow. \n\nThank you. \n\n`);
+// }
+// console.log(`Mrs ${not_coming},is not coming for dinner tommorrow`)
+// guest_list.unshift(`Iqra`);
+// guest_list.splice(guest_list.length/2, 2, "Hira");
+// guest_list.push("Mahnoor");
+// for(let i=0; i< guest_list.length; i++){
+//     console.log(`Dear ` + guest_list[i] +`\n\nYou are invited for a dinner tommorrow. \n\nThank you. \n\n`);
+// }
+// console.log(`we have found a bigger dinner table we so invited more guests `);
+// console.log(`\n\nunfortunately we are out of space,so have space for two guests only.\n\n `);
+// while(guest_list.length>2){
+//     let removed_guest = guest_list.pop();
+//     console.log(`sorry, Dear ${removed_guest}, you are not invited for a dinner tommorrow`);
+// }
+// for(let i=0; i< guest_list.length; i++){
+//     console.log(`Dear ` + guest_list[i] +`\n\nYou all are still invited for a dinner tommorrow. \n\nThank you. \n\n`);
+// }
+// guest_list.splice(0,2);
+// console.log(guest_list);
