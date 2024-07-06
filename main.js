cc50ba63b0a18423e969d8c840ebb5957c8d7685
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:34:24 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+var guest_list = ["maryam", "neha", "alishba"];
+exports.guest_list = guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Dear " + guest_list[i] + "\n\nYou are invited for a dinner tommorrow. \n\nThank you. \n\n");
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+let guest_list : string[] = [`maryam`,`neha`,`alishba`];
+for(let i=0; i< guest_list.length; i++){
+    console.log(`Dear ` + guest_list[i] +`\n\nYou are invited for a dinner tommorrow. \n\nThank you. \n\n`);
+}
+
+export{guest_list}
