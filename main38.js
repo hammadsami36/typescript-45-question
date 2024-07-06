cedbb24dd8f72625a0a41e8881ada533b0ef55ac
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:53:24 +0500

    Add files via upload

---
+++ main38.js
@@ -0,0 +1,9 @@
+var karachi = "karachi is in Pakistan";
+var paris = "paris is in france";
+var barcelona = "barcelona is in spain";
+function describe_city(city) {
+    console.log(city);
+}
+console.log(karachi);
+console.log(paris);
+console.log(barcelona);
---
+++ main38.ts
@@ -0,0 +1,11 @@
+let karachi = "karachi is in Pakistan"
+let paris = "paris is in france"
+let barcelona = "barcelona is in spain"
+
+function describe_city(city:string,){
+    console.log(city)
+}
+console.log(karachi)
+console.log(paris)
+console.log(barcelona)
+
