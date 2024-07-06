095ce2e8701d2ba445c39eb39e85b6e01ab5f414
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:50:38 +0500

    Add files via upload

---
+++ main34.js
@@ -0,0 +1,5 @@
+var pizzas = ["Pepperoni", "Italian", "bbq chicken"];
+for (var i = 0; i < pizzas.length; i++) {
+    console.log("I like ".concat(pizzas[i], " pizza"));
+}
+console.log("I really love pizza!");
---
+++ main34.ts
@@ -0,0 +1,7 @@
+let pizzas : string[]=["Pepperoni","Italian","bbq chicken"];
+
+for (let i=0; i<pizzas.length; i++ ){
+
+    console.log(`I like ${pizzas[i]} pizza`);
+}
+console.log("I really love pizza!");
