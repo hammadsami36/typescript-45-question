4ec94df994a58f0c295fc3593dc1e0d5458fe6f8
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:46:56 +0500

    Add files via upload

---
+++ main29.ts
@@ -0,0 +1,17 @@
+let favorite_fruit = ["mango","banana","apple"];
+
+if (favorite_fruit.includes("mango")){
+    console.log("you really like mango!");
+}
+if (favorite_fruit.includes("banana")){
+    console.log("you really like banana!");
+}
+if (favorite_fruit.includes("apple")){
+    console.log("you really like apple!");
+}
+if (favorite_fruit.includes("grapes")){
+    console.log("you really like grapes!");
+}
+if (favorite_fruit.includes("orange")){
+    console.log("you really like orange!");
+}
