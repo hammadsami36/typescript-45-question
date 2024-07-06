860db2b7bf1350990fd2417562d7ecb60624d3fd
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:32:14 +0500

    Add files via upload

---
+++ main11.js
@@ -0,0 +1,11 @@
+// Question 11
+// let friends : string[] = [`ayesha`,`alishba`,`iqra`];
+// for(let i=0; i<friends.length; i++){
+// console.log(friends[i]);
+// }
+// Question 12
+var friends = ["ayesha", "alishba", "iqra"];
+var message = "You are invited for a lanch tommorrow ";
+for (var i = 0; i < friends.length; i++) {
+    console.log(message + friends[i]);
+}
---
+++ main11.ts
@@ -0,0 +1,12 @@
+// Question 11
+// let friends : string[] = [`ayesha`,`alishba`,`iqra`];
+// for(let i=0; i<friends.length; i++){
+// console.log(friends[i]);
+// }
+
+// Question 12
+let friends : string[] = [`ayesha`,`alishba`,`iqra`];
+let message : string = `You are invited for a lanch tommorrow `;
+for(let i=0; i<friends.length; i++){
+console.log(message+friends[i]);
+}
