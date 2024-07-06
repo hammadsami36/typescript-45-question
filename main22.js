d6dc7323e7bad505835aa98745b0e7a74c3b5c8c
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:41:15 +0500

    Add files via upload

---
+++ main22.js
@@ -0,0 +1,6 @@
+var listing = ["ayesha", "maryam", "alishba", "iqra", "amber"];
+console.log(listing[4]);
+console.log(listing[0]);
+console.log(listing[1]);
+console.log(listing[2]);
+console.log(listing[3]);
---
+++ main22.ts
@@ -0,0 +1,6 @@
+let listing : string[] = [`ayesha`,`maryam`,`alishba`,`iqra`,`amber`]
+console.log(listing[4])
+console.log(listing[0])
+console.log(listing[1])
+console.log(listing[2])
+console.log(listing[3])
