9575221b9c62d4568a28fef2689283165d3aa537
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:42:05 +0500

    Add files via upload

---
+++ main23.ts
@@ -0,0 +1,44 @@
+let car = 'subaru';
+// // 5 subaru Tests True
+// // equality Test
+
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru'); 
+
+
+
+// // strick equality test
+console.log("Is car === 'subaru'? I predict True.")
+console.log(car === 'subaru');
+
+
+// Inequality test
+console.log("Is car != 'toyota'? I predict True.")
+console.log(car != 'toyota');
+
+console.log("Is car != 'ford'? I predict True.")
+console.log(car != 'ford');
+
+console.log("Is car != 'suzuki'? I predict True.")
+console.log(car != 'suzuki');
+
+//subaru Tests false 
+console.log("Is car == 'ford'? I predict True.")
+console.log(car == 'ford');
+
+
+console.log("Is car == 'suzuki'? I predict True.")
+console.log(car == 'suzuki');
+
+
+console.log("Is car != 'subaru'? I predict True.")
+console.log(car != 'subaru'); 
+
+
+console.log("Is car != ''? I predict True.")
+console.log(car != 'subaru');
+
+
+
+console.log("Is car == 'toyota'? I predict True.")
+console.log(car == 'toyota');
