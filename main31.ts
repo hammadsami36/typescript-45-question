fde3989cd1fc385a51b87910af6d1c8e4ebb8647
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:48:10 +0500

    Add files via upload

---
+++ main31.ts
@@ -0,0 +1,8 @@
+let userName : string [] = ["admin","ayesha","maryam","amber","alishba"];
+
+if(userName.length > 0){
+    console.log("we have enough users");
+}else{
+    userName = [];
+    console.log("we need to some find users!");
+}
