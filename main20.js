07b9602df20ea98531071f180f53a41ca777a0da
hammadsami36 <www.samihammad9991@gmail.com>
2024-07-06 21:39:49 +0500

    Add files via upload

---
+++ main20.js
@@ -0,0 +1,5 @@
+var mountain_list = ["k2", "himalayas", "cho oyu", "nanga parbat", "babcock peak"];
+console.log("\nlist of Mountain");
+for (var i = 0; i < mountain_list.length; i++) {
+    console.log("".concat(mountain_list[i]));
+}
---
+++ main20.ts
@@ -0,0 +1,5 @@
+let mountain_list: string [] = [`k2`,`himalayas`,`cho oyu`,`nanga parbat`,`babcock peak`]
+console.log(`\nlist of Mountain`)
+for (let i=0; i<mountain_list.length; i++){
+    console.log(`${mountain_list[i]}`)
+}
